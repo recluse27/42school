d63b0ca3e0ea458ac175219dfd4cf14c629a6a5f
Vitalii Lazuka <vlazuka@e1z3r4p23.42.us.org>
2018-09-11 18:18:47 -0700

    sdfm

--- 42sc/progr/b_libft/ft_strncpy.c
+++ 42sc/progr/b_libft/ft_strncpy.c
@@ -6,22 +6,33 @@
 /*   By: vlazuka <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/10 18:06:22 by vlazuka           #+#    #+#             */
-/*   Updated: 2018/09/10 22:24:54 by vlazuka          ###   ########.fr       */
+/*   Updated: 2018/09/11 18:18:14 by vlazuka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strncpy(char *dest, char *src, unsigned int n)
+char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
-	unsigned int i;
+//	unsigned int i;
+//
+//	i = 0;
+//	while (src[i] != '\0' && i < n)
+//	{
+//		dest[i] = src[i];
+//		i++;
+//	}
+//	dest[i] = '\0';
+//	return (dest);
+	size_t	i;
 
 	i = 0;
-	while (src[i] != '\0' && i < n)
+	while (src[i] && i < len)
 	{
-		dest[i] = src[i];
+		dst[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
-	return (dest);
+	while (i < len)
+		dst[i++] = '\0';
+	return (dst);
 }
--- 42sc/progr/b_libft/libft.h
+++ 42sc/progr/b_libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: vlazuka <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/11 09:49:17 by vlazuka           #+#    #+#             */
-/*   Updated: 2018/09/11 09:49:19 by vlazuka          ###   ########.fr       */
+/*   Updated: 2018/09/11 18:16:03 by vlazuka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memset(void *b, int c, size_t len);
 size_t	ft_strlen(const char *str);
 char	*ft_strdup(const char *s1);
 char	*ft_strcpy(char *dst, const char *src);
-char	*ft_strncpy(char *dest, char *src, unsigned int n);
+char	*ft_strncpy(char *dest, const char *src, size_t len);
 char	*ft_strchr(const char *s, int c);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_isdigit(int c);
