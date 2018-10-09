06e310adcc7d2ef96bc426bffaad7cb816e1f533
Vitalii Lazuka <vlazuka@e1z3r7p19.42.us.org>
2018-10-09 15:13:02 -0700

    hfg

--- 42sc/progr/bbb_ls/b_ls.c
+++ 42sc/progr/bbb_ls/b_ls.c
@@ -10,29 +10,69 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <dirent.h>
+// #include <sys/types.h>
+// #include <sys/stat.h>
+// #include <unistd.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <dirent.h>
 
-int		main(int argc, char *argv[])
-{
-	DIR *mydir;
-	struct dirent *myfile;
-	struct stat mystat;
+// int		main(int argc, char *argv[])
+// {
+// 	if (argc != 0)
+// 	{
+// 		DIR *mydir;
+// 		struct dirent *myfile;
+// 		struct stat mystat;
 
-	char buf[512];
-	mydir = opendir(argv[1]);
-	while((myfile = readdir(mydir)) != NULL)
+// 		char buf[512];
+// 		mydir = opendir(argv[1]);
+// 		while((myfile = readdir(mydir)) != NULL)
+// 		{
+// 			sprintf(buf, "%s/%s", argv[1], myfile->d_name);
+// 			stat(buf, &mystat);
+// 			printf("%zu", mystat.st_size);
+// 			printf(" %s\n", myfile->d_name);
+// 		}
+// 		closedir(mydir);
+// 	}
+
+// 	return (0);
+// }
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <sys/types.h> 
+#include <dirent.h> 
+  
+int main(void) 
+{ 
+	char *curr_dir = NULL; 
+	DIR *dp = NULL; 
+	struct dirent *dptr = NULL; 
+	unsigned int count = 0; 
+
+	curr_dir = getenv("PWD"); 
+	if(NULL == curr_dir) 
+	{ 
+		printf("\n ERROR : Could not get the working directory\n"); 
+		return (-1); 
+	} 
+
+	dp = opendir((const char*)curr_dir);
+	if (NULL == dp)
 	{
-		sprintf(buf, "%s/%s", argv[1], myfile->d_name);
-		stat(buf, &mystat);
-		printf("%zu", mystat.st_size);
-		printf(" %s\n", myfile->d_name);
+		printf("\n ERROR : Could not open the working directory\n");
+		return (-1);
 	}
-	closedir(mydir);
 
-	return (0);
+	printf("\n"); 
+	for (count = 0; NULL != (dptr = readdir(dp)); count++) 
+	{ 
+		// Check if the name of the file/folder begins with '.' 
+		// If yes, then do not display it. 
+		if(dptr->d_name[0] != '.') 
+			printf("%s  ",dptr->d_name); 
+	}
+
+	return (0); 
 }
