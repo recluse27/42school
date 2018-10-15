53796e72e9b73a36cd68382cc74544bd2ac68a02
Vitalii Lazuka <vlazuka@e1z2r6p13.42.us.org>
2018-10-15 15:44:22 -0700

    hdshsdh

--- 42sc/progr/my_ls.c
+++ 42sc/progr/my_ls.c
@@ -40,6 +40,7 @@ int A = 0;
 int L = 0;
 int R = 0;
 int T = 0;
+char *dirpath = '.';
 
 # define O_FORMAT	options[0]
 # define O_SORT		options[1]
@@ -49,6 +50,72 @@ int T = 0;
 # define O_REVERS	options[5]
 
 #endif
+
+int ft_strlen(const char *str)
+{
+	int i = 0;
+	while(str[i])
+		i++;
+	return i;
+}
+
+int		ft_strcmp(const char *s1, const char *s2)
+{
+	while (*s1 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
+}
+
+void b_ls(int i, char **argv)
+{
+	struct stat buf;
+	struct dirent *sd = NULL;
+	node *head;
+	long long int blocks = 0;
+	char *dirpath;
+	DIR *dir;
+
+	if(argv[i] == NULL)
+		dirpath = ".";
+	else
+		dirpath = argv[i];
+	if((dir = opendir(dirpath)) == NULL)
+	{
+		printf("b_ls: %s: No Such a file or directory.", argv[i]);
+		return ;
+	}
+	if(L == 1)
+	{
+		while((sd = readdir(dir)))
+		{
+			if((sd->d_name[0] == '.' && A == 1) || (sd->d_name[0] != '.'))
+			{
+				lstat(getpath(dirpath, sd->d_name), &buf);
+				blocks += buf.st_blocks;
+			}
+		}
+		printf("total %lld\n", blocks);
+		closedir(dir);
+	}
+	dir = opendir(dirpath);
+	while((sd = readdir(dir)))
+	{
+		lstat(getpath(dirpath, sd->d_name), &buf);
+		head = insertend(head, sd, buf);
+	}
+	n_sort(head);
+	if(T == 1)
+		t_sort(head);
+	while(head)
+	{
+		printer(head, dirpath);
+		head = head->next;
+	}
+	closedir(dir);
+}
 
 int main(int argc, char *argv[])
 {
