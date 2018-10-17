fb73ba570a0ca65cb4c4f3dda71a1c0ecf116e99
Vitalii Lazuka <vlazuka@e1z3r7p23.42.us.org>
2018-10-16 17:34:45 -0700

    hsdg

--- 42sc/progr/my_ls copy.c
+++ 42sc/progr/my_ls copy.c
@@ -1,2 +1,15 @@
 #ifndef
 # define B_LS_H
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/stat.h>
+# include <pwd.h>
+# include <grp.h>
+# include <time.h>
+# include <dirent.h>
+# include <stdlib.h>
+
+int A = 0;
+int L = 0;
+int R = 0;
+int T = 0;
